89d63eccac0f315d6f679e2278de2a98388e6720
oneekov <73463642+oneekov@users.noreply.github.com>
2025-07-25 04:26:47 +0300

    another fix

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from lib.config import *
 from lib.api import api
 from lib.views import *
 
+with db.session() as session:
+    if not (session.execute(select(exists().where(User.id==1)))).scalar():
+        root = User(id=1, username=ROOT_USER, password_hash=ROOT_PASSWORD_HASH, is_super_admin=True, surname="Супер", name="Админ", patronymic="Великолепный")
+        session.execute(text("SELECT setval('klimov.users_id_seq', 1)"))
+        session.add(root)
+        session.commit()
+
 app.register_blueprint(api)
 
 @app.route('/<path:filename>')
@@ -21,10 +28,4 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    with db.session() as session:
-        if not (session.execute(select(exists().where(User.id==1)))).scalar():
-            root = User(id=1, username=ROOT_USER, password_hash=ROOT_PASSWORD_HASH, is_super_admin=True, surname="Супер", name="Админ", patronymic="Великолепный")
-            session.execute(text("SELECT setval('klimov.users_id_seq', 1)"))
-            session.add(root)
-            session.commit()
     app.run(host=HOST, port=PORT, debug=True)
